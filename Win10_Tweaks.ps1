9e3334872da5b67aa6b72e286920b04e17a20223
wilcoxk <wilcoxk@wws.k12.in.us>
2018-05-15 15:05:17 -0400

    Extra backup may 2018

---
+++ Win10_Tweaks.ps1
@@ -0,0 +1,5 @@
+﻿Copy-Item .\shortcuts\* C:\Users\Public\Desktop -recurse
+
+Copy-Item .\SampleMedia C:\software -recurse
+
+Import-StartLayout -LayoutPath .\start_taskbar_layout.xml -MountPath $env:SystemDrive\
